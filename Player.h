5db17616b9c7ae2a48c7e2ba6f91d81820656437
Your Name <gwen.hurtt@nwktc.edu>
2012-04-25 11:41:48 -0700

    Placed a Player class.

---
+++ Player.h
@@ -0,0 +1,15 @@
+//
+//  Player.h
+//  BlindMansBluff
+//
+//  Created by Gwen Hurtt on 4/25/12.
+//  Copyright (c) 2012 NWKTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Player : UIViewController
+@property BOOL PlayerIsIn;
+
+
+@end
