48cc353eac7f8230126a8dbeaf71a7a186286eff
Your Name <gwen.hurtt@nwktc.edu>
2012-04-27 13:29:37 -0700

    see if this works class

---
+++ Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  BlindMansBluff
+//
+//  Created by Gwen Hurtt on 4/27/12.
+//  Copyright (c) 2012 NWKTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Player : UIView
+
+@end
---
+++ Player.m
@@ -0,0 +1,31 @@
+//
+//  Player.m
+//  BlindMansBluff
+//
+//  Created by Gwen Hurtt on 4/27/12.
+//  Copyright (c) 2012 NWKTC. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
